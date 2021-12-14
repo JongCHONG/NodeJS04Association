1625909f5a3a343367086a775b34ff37d2a9f5c1
unknown <jochong27@gmail.com>
2021-12-14 22:46:49 +0100

    update

--- backend/routes/associations.js
+++ backend/routes/associations.js
@@ -1,6 +1,41 @@
 const express = require("express")
+const fs = require('fs');
 const app = express()
 
-let associations = require("../associations")
+const associations = require("../associations")
+let messages = require("../messages")
+
+const checkExistingAssoc = (req, res, next) => {
+  const { name } = req.params
+  
+  const association = associations.find(element => element.name === name)
+
+  if(association) {
+    next()
+  } else {
+    res.status(404).send("Association not found.")
+  }
+}
+
+app.get("/", (req, res) => {
+  res.json(associations)
+})
+
+app.get("/:name", checkExistingAssoc, (req, res) => {
+  const { name } = req.params
+
+  const association = associations.find(element => element.name === name)
+
+  res.json(association)
+})
+
+app.post("/", checkExistingAssoc, (req, res) => {
+  const message = req.body
+
+  messages = { ...req.body, message }
+  
+  fs.writeFile('../messages', JSON.stringify(messages))
+
+})
 
 module.exports = app
