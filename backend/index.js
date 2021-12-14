14d86bd834bd9677f52a8de2f0725bf295614989
unknown <jochong27@gmail.com>
2021-12-14 17:04:30 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const morgan = require ("morgan")
+// const cors = require("cors")
+const app = express()
+const port = 5000
+
+//relier back et front
+// app.use(cors())
+
+//import json
+const associations = require("./routes/associations")
+
+//accéder à req.body
+app.use(express.json())
+
+//middleware gobal des requetes
+app.use(morgan('tiny'))
+
+app.use("/associations", associations)
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
---
+++ backend/routes/associations.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const app = express()
+
+let associations = require("../associations")
+
+module.exports = app
